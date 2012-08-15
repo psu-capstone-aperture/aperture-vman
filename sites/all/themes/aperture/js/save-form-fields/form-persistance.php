9e98097acceb89d2348120954997d40f09a7bc95
Ruvim Micsanschi <mruvim@pdx.edu>
2012-08-15 02:10:18 -0700

    jQuery script for saving data in the form fields

---
+++ sites/all/themes/aperture/js/save-form-fields/form-persistance.php
@@ -0,0 +1,8 @@
+ <script src="sites/all/themes/aperture/js/save-form-fields/jquery-1.5.min.js"></script>
+ <script src="sites/all/themes/aperture/js/save-form-fields/json2.js"></script>
+ <script src="sites/all/themes/aperture/js/save-form-fields/dumbFormState-1.js"></script>
+ <script>
+            jQuery(function(){ // ensure dom is loaded first
+                jQuery('form').dumbFormState();
+            });
+</script>
