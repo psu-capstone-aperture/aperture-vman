604e2b83406fcef982f5391b8b844fdbad69d473
Ruvim Micsanschi <mruvim@pdx.edu>
2012-08-22 14:22:05 -0700

    Fix for the "Run Report" page to show date picker and save date in the fields

--- sites/all/themes/aperture/js/save-form-fields/form-persistance.php
+++ sites/all/themes/aperture/js/save-form-fields/form-persistance.php
@@ -1,6 +1,6 @@
- <script src="sites/all/themes/aperture/js/save-form-fields/jquery-1.5.min.js"></script>
- <script src="sites/all/themes/aperture/js/save-form-fields/json2.js"></script>
- <script src="sites/all/themes/aperture/js/save-form-fields/dumbFormState-1.js"></script>
+ <script src="../sites/all/themes/aperture/js/save-form-fields/jquery-1.5.min.js"></script>
+ <script src="../sites/all/themes/aperture/js/save-form-fields/json2.js"></script>
+ <script src="../sites/all/themes/aperture/js/save-form-fields/dumbFormState-1.js"></script>
  <script>
             jQuery(function(){ // ensure dom is loaded first
                 jQuery('form').dumbFormState();
